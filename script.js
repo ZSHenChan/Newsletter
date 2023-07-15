9a3ccc16eb0804d96c509fec592ea1adcc270516
Zhen <135425829+ZSHenChan@users.noreply.github.com>
2023-07-15 12:26:06 +0800

    completed submit button with email validation
    Yay!

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const form = document.getElementById("form");
 const userEmail = document.getElementById('user-email');
+const invalidMsg = document.getElementById('invalid-msg')
+
+function validateEmail(){
+    if (userEmail.value.match(/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/)){
+        invalidMsg.style.display = "none";
+        return true;
+    }
+    else{
+        invalidMsg.style.display = "flex";
+        return false;
+    }
+}
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
-    const userEmailValue = userEmail.value;
+    if (validateEmail()){
+        const userEmailValue = userEmail.value;
 
-    localStorage.setItem("userEmail",userEmailValue);
-    window.location.href = "success.html";
+        localStorage.setItem("userEmail",userEmailValue);
+        window.location.href = "success.html";
+    }
+    
 })  
