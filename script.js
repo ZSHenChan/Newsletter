88a1d909ad3f39e975ce23f41a7215e52054fa31
Zhen <135425829+ZSHenChan@users.noreply.github.com>
2023-07-15 12:09:44 +0800

    external js file

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function getEmail(){
-    const form = document.getElementById("form");
-        const userEmail = document.getElementById('user-email');
-    
-        form.addEventListener('submit',function(e){
-            e.preventDefault();
-    
-            const userEmailValue = userEmail.value;
-    
-            localStorage.setItem("userEmail",userEmailValue);
-            window.location.href = "success.html";
-        })  
-}
+const form = document.getElementById("form");
+const userEmail = document.getElementById('user-email');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const userEmailValue = userEmail.value;
+
+    localStorage.setItem("userEmail",userEmailValue);
+    window.location.href = "success.html";
+})  
